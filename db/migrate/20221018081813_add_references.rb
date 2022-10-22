9060acd6781cf582532ac6ac0be4eb15dec30695
Kweku Bright <kweku01bright@gmail.com>
2022-10-22 09:07:07 +0100

    linter fix

--- db/migrate/20221018081813_add_references.rb
+++ db/migrate/20221018081813_add_references.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddReferences < ActiveRecord::Migration[7.0]
   def change
     add_reference :user_transactions, :user, null: false, foreign_key: true, on_delete: :cascade
--- db/migrate/20221018081945_rename_columns.rb
+++ db/migrate/20221018081945_rename_columns.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameColumns < ActiveRecord::Migration[7.0]
   def change
     rename_column :groups, :user_id, :author_id
